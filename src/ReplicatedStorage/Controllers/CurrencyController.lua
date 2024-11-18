7fb22326c1207e1a37728aca7c1b531a0f0c0f08
Ozzy Callooh <ozzycallooh@gmail.com>
2024-11-17 19:07:48 -0500

    Add newservice, getservice, getcontroller snippets

---
+++ src/ReplicatedStorage/Controllers/CurrencyController.lua
@@ -0,0 +1,14 @@
+--!strict
+
+--[[
+	Client-side currency controller
+]]
+local CurrencyController = {}
+
+function CurrencyController.init(self: CurrencyController)
+	-- TODO
+end
+
+export type CurrencyController = typeof(CurrencyController)
+
+return CurrencyController
